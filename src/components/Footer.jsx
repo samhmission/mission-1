ba802c02c6de8ab41bf11061f22fb369f0bef17a
Sam H <samuelh@missionreadyhq.com>
2024-11-21 16:40:52 +1300

    Update footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2024 Vehicle Insurance Predictor. All Rights Reserved.</p>
+      <p>&copy; 2024 Turners Car Auctions. All Rights Reserved.</p>
     </footer>
   );
 };
